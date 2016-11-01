2aacbfbce8c285cb09c500ef2b0b669c1813aa68
weiyisheng <weiyisheng1991@icloud.com>
2016-11-01 15:56:39 +0800

    change

---
+++ ios/ReactNativeComponentSnapShot/ReactNativeComponentSnapShot/SnapShotViewManager.h
@@ -0,0 +1,13 @@
+//
+//  SnapShotViewManager.h
+//  ReactNativeComponentSnapShot
+//
+//  Created by gemini on 11/1/16.
+//  Copyright © 2016 gemini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnapShotViewManager : RCTView
+
+@end
---
+++ ios/ReactNativeComponentSnapShot/ReactNativeComponentSnapShot/SnapShotViewManager.m
@@ -0,0 +1,13 @@
+//
+//  SnapShotViewManager.m
+//  ReactNativeComponentSnapShot
+//
+//  Created by gemini on 11/1/16.
+//  Copyright © 2016 gemini. All rights reserved.
+//
+
+#import "SnapShotViewManager.h"
+
+@implementation SnapShotViewManager
+
+@end
